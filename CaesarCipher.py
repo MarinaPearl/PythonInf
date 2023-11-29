30956a887e777f98f63337c7d9fec3aba339888b
MarinaPearl <marina.demchuk.01@bk.ru>
2023-11-30 01:32:44 +0700

    added caesar cipher

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,6 +1,6 @@
 
-def caesar_cipher(text, shift, language):
-    result = ""
+def caesar_cipher(input_text, shift, language):
+    result_coder = ""
     alphabet = ""
 
     if language.lower() == "en":
@@ -11,37 +11,37 @@ def caesar_cipher(text, shift, language):
         print("Нет такого языка...")
         return
 
-    for char in text:
+    for char in input_text:
         if char.upper() in alphabet:
             is_upper = char.isupper()
             index = (alphabet.index(char.upper()) + shift) % len(alphabet)
             if is_upper:
-                result += alphabet[index]
+                result_coder += alphabet[index]
             else:
-                result += alphabet[index].lower()
+                result_coder += alphabet[index].lower()
         else:
-            result += char
+            result_coder += char
 
-    return result
+    return result_coder
 
 
-def write_file():
+def write_to_file():
     try:
         input_file_path = input("Введите путь до файла: ")
-        shift = int(input("Введите сдвиг: "))
+        shift = int(input("Введите сдвиг(число): "))
         language = input("Введите язык (en/ru): ")
 
         with open(input_file_path, "r", encoding="utf-8") as file:
-            text = file.read()
+            input_text = file.read()
 
-        encrypted_text = caesar_cipher(text, shift, language)
+        encrypted_text = caesar_cipher(input_text, shift, language)
 
         with open("output.txt", "w", encoding="utf-8") as file:
             file.write(encrypted_text)
 
     except Exception as e:
-        print("УУПС, что-то пошло не так", e)
+        print("Ошибка, попробуйте снова", e)
 
 
 if __name__ == "__main__":
-    write_file()
+    write_to_file()
